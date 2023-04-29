dfdab89e2af0ca6a2c0bd6b15da4b057f07439b0
jz769491062 <zihanzhu@usc.edu>
2023-04-29 10:05:41 -0700

    BFS专项4-双向多源BFS

---
+++ Code/BFS/1162. 地图分析.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.cn/problems/as-far-from-land-as-possible/description/
+class Solution {
+public:
+    static constexpr int MAX_N = 100 + 5;
+    static constexpr int INF = int(1E6);
+    static constexpr int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+    int n;
+    int d[MAX_N][MAX_N];
+
+    struct Coordinate {
+        int x, y;
+    };
+
+    queue <Coordinate> q;
+
+    int maxDistance(vector<vector<int>>& grid) {
+        this->n = grid.size();
+        auto &a = grid;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d[i][j] = INF;
+            }
+        }
+        // 从陆地开始多源BFS
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (a[i][j]) {
+                    d[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto f = q.front(); q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nx = f.x + dx[i], ny = f.y + dy[i];
+                if (!(nx >= 0 && nx <= n - 1 && ny >= 0 && ny <= n - 1)) continue;
+                if (d[nx][ny] > d[f.x][f.y] + 1) {
+                    d[nx][ny] = d[f.x][f.y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (!a[i][j]) {
+                    ans = max(ans, d[i][j]);
+                }
+            }
+        }
+
+        return (ans == INF) ? -1 : ans;
+    }
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/as-far-from-land-as-possible/solutions/147423/di-tu-fen-xi-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
